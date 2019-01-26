bd9c051415670573b9a8e5e719fefd164d4601f3
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-26 17:24:02 +0900

    Brew formula update for cli-sample version v1.0.3

--- cli-sample.rb
+++ cli-sample.rb
@@ -1,17 +1,11 @@
-require "formula"
-
-HOMEBREW_CLI_SAMPLE_VERSION='1.0.2'
-
 class CliSample < Formula
-  url "https://github.com/fox-kazuki-matsumoto/cli-sample/releases/download/v#{HOMEBREW_CLI_SAMPLE_VERSION}/cli-sample_#{HOMEBREW_CLI_SAMPLE_VERSION}_darwin_amd64.tar.gz"
+  desc ""
   homepage "https://github.com/fox-kazuki-matsumoto/cli-sample"
-  sha256 "6812737673ba79d61df3f00399f17f0c98527938a845b5d8b08728d591242b09"
-
-  version "v#{HOMEBREW_CLI_SAMPLE_VERSION}"
-  head 'https://github.com/fox-kazuki-matsumoto/cli-sample.git', :branch => 'master'
+  url "https://github.com/fox-kazuki-matsumoto/cli-sample/releases/download/v1.0.3/cli-sample_1.0.3_darwin_amd64.tar.gz"
+  version "1.0.3"
+  sha256 "4a5f1d9296cba23d6d382c8077456db1a23885b90a8906ff1b84b986bf080102"
 
   def install
-    bin.install 'cli-sample'
+    bin.install "cli-sample"
   end
-
 end

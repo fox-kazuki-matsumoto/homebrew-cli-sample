1b5b69c8fed19842aada1aeb72cbb1e90c836694
fox-kazuki-matsumoto <kazuki.matsumoto@fox-hound.jp>
2019-01-26 15:28:46 +0900

    Update sha256

--- cli-sample.rb
+++ cli-sample.rb
@@ -5,7 +5,7 @@ HOMEBREW_CLI_SAMPLE_VERSION='1.0.2'
 class CliSample < Formula
   url "https://github.com/fox-kazuki-matsumoto/cli-sample/releases/download/v#{HOMEBREW_CLI_SAMPLE_VERSION}/cli-sample_#{HOMEBREW_CLI_SAMPLE_VERSION}_darwin_amd64.tar.gz"
   homepage "https://github.com/fox-kazuki-matsumoto/cli-sample"
-  sha256 "c3d8fb12347b1461a800e31e12600cfe4b556fcc74ec0dfcf97d8c41476a3bfa"
+  sha256 "6812737673ba79d61df3f00399f17f0c98527938a845b5d8b08728d591242b09"
 
   version "v#{HOMEBREW_CLI_SAMPLE_VERSION}"
   head 'https://github.com/fox-kazuki-matsumoto/cli-sample.git', :branch => 'master'
